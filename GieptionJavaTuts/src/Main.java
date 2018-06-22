402b9920d987eafb9298e8d712f8736555731c33
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-22 20:34:46 +0800

    Quick commit 3

--- GieptionJavaTuts/src/Main.java
+++ GieptionJavaTuts/src/Main.java
@@ -1,13 +1,20 @@
-
 public class Main {
+
+	private static String name = "Global Variable";  // static, can be called anywhere in the class,
+														// basically .h header file of a C++ class
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Global Variable " + name);
+		
+		String name2= "Local Variable"; 
+		System.out.println("Local Variable "+name2);
 		
 	for (int i = 0 ; i<5; i++) {
 		System.out.println(i);
 		
 	}
+	
 	int i = 2;
 		while(i<100)
 		{
@@ -18,15 +25,16 @@ public class Main {
 		
 		System.out.println(add(2,3));
 	}
+	
 	private static int Linear( int m , int x, int c) {
 		int y = m + x + c;
 		
 		return y;
 	}
 	
-	private  int add( int a , int b) {
+	private static  int add( int a , int b) {
 		int c = a+b;
 		return c;
-		
 	}
+	
 }
