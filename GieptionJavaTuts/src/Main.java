756dc7867ddd715d8bf09a9fd249539d761a32f6
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-22 20:24:15 +0800

    Quick Commit 2

--- GieptionJavaTuts/src/Main.java
+++ GieptionJavaTuts/src/Main.java
@@ -3,12 +3,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
 	for (int i = 0 ; i<5; i++) {
 		System.out.println(i);
 		
 	}
+	int i = 2;
+		while(i<100)
+		{
+			i=i+5;
+			System.out.println(i);
+		}
+		System.out.println(Linear(2,3,10));
+		
+		System.out.println(add(2,3));
 	}
-
+	private static int Linear( int m , int x, int c) {
+		int y = m + x + c;
+		
+		return y;
+	}
+	
+	private  int add( int a , int b) {
+		int c = a+b;
+		return c;
+		
+	}
 }
