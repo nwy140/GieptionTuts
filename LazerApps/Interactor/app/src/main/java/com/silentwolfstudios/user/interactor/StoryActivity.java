173a5329ca8d44632aa0e1a3de7d1c9c7eb9534b
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-23 16:49:26 +0800

    Changed Android to tools in xml
     <!--tools means in android studio only, src will be seen in ide, but not in actual build-->

--- LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/StoryActivity.java
+++ LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/StoryActivity.java
@@ -15,7 +15,12 @@ public class StoryActivity extends AppCompatActivity {
     Intent i = getIntent();
     String name = i.getStringExtra(getResources().getString(R.string.key)); //get string from key
     Toast.makeText(this, name , Toast.LENGTH_SHORT).show();
+
+    loadPage();
     }
 
+    private void loadPage(int p ageNumber){
+
+    }
     //mvp model view presenter //3 models // each rows eg. button tv and iv used for navigation // entire layout is third model
 }
