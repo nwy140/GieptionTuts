adf8734a70b771d5b5465fa7a8a2608a67f1de7c
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-23 15:07:01 +0800

    Set OnClickListener to etName and ivMainTitle

--- LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/MainActivity.java
+++ LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/MainActivity.java
@@ -1,13 +1,54 @@
 package com.silentwolfstudios.user.interactor;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity { // ctrl  + click is f12 in visual studio
+
+    private EditText etName;
+    private Button btStartAdventure;
+    private ImageView ivMainTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // saveInstanceState , saved state of app
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        btStartAdventure = findViewById(R.id.btStartAdventure);
+
+        btStartAdventure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name = etName.getText().toString();
+                Toast.makeText(MainActivity.this, Name, Toast.LENGTH_SHORT).show();
+            }
+        });
+        ivMainTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(view);
+            }
+        });
+
+
     }
+
+    private  void hideKeyboard(View view){
+        view =getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+
 }
