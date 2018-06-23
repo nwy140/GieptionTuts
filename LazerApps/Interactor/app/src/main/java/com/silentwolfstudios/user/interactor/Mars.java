7f6f5390369fce9231ed5c25464e4fafe5f53733
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-23 18:04:46 +0800

    Used Page Stack

--- LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/Mars.java
+++ LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/Mars.java
@@ -13,22 +13,22 @@ public class Mars {
 
         pages[1] = new Page(R.string.page1,
                 R.drawable.page1,
-                new Choice(R.string.page0_choice1,3),
+                new Choice(R.string.page1_choice1,3),
                 new Choice(R.string.page1_choice2,4));
 
         pages[2] = new Page(R.string.page2,
                 R.drawable.page2,
-                new Choice(R.string.page0_choice1,4),
+                new Choice(R.string.page2_choice1,4),
                 new Choice(R.string.page2_choice2,6));
 
         pages[3] = new Page(R.string.page3,
                 R.drawable.page3,
-                new Choice(R.string.page0_choice1,4),
+                new Choice(R.string.page3_choice1,4),
                 new Choice(R.string.page3_choice2,5));
 
         pages[4] = new Page(R.string.page4,
                 R.drawable.page4,
-                new Choice(R.string.page0_choice1,5),
+                new Choice(R.string.page4_choice1,5),
                 new Choice(R.string.page4_choice2,6));
 
         pages[5] = new Page(R.string.page5,R.drawable.page5);
--- LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/StoryActivity.java
+++ LazerApps/Interactor/app/src/main/java/com/silentwolfstudios/user/interactor/StoryActivity.java
@@ -11,13 +11,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Stack;
+
 public class StoryActivity extends AppCompatActivity {
 
     private Mars mars;
+    private String name;
+
     private ImageView ivStoryScene;
     private TextView tvStoryText;
     private Button btStoryChoice1;
     private Button btStoryChoice2;
+
+    private Stack<Integer> pageStack = new Stack<>();
 
 
     @Override
@@ -31,7 +37,7 @@ public class StoryActivity extends AppCompatActivity {
         btStoryChoice2 = findViewById(R.id.btStoryChoice2);
 
     Intent i = getIntent();
-    String name = i.getStringExtra(getResources().getString(R.string.key)); //get string from key
+    name = i.getStringExtra(getResources().getString(R.string.key)); //get string from key
 
         if (name==null|| name.isEmpty()){
             name="friend";
@@ -44,39 +50,61 @@ public class StoryActivity extends AppCompatActivity {
     }
 
     private void loadPage(int pageNumber){
+
+        pageStack.push(pageNumber);
+
         final Page page = mars.getPage(pageNumber);
         Drawable drawable = getResources().getDrawable(page.getImageId()); // get a drawable image from res
         ivStoryScene.setImageDrawable(drawable);
-        tvStoryText.setText(page.getTextId());
+        tvStoryText.setText(String.format(getString(page.getTextId()), name));//passed user's name
 
         if(page.isFinalPage()){
             btStoryChoice2.setText("Play Again");
+            btStoryChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(0);
+                }
+            });
             btStoryChoice1.setVisibility(View.INVISIBLE);
         } else{
-            btStoryChoice2.setText(page.getChoice1().getButtonId());
-            btStoryChoice1.setText(page.getChoice2().getButtonId());
             btStoryChoice1.setVisibility(View.VISIBLE);
+            btStoryChoice2.setText(page.getChoice2().getButtonId());
+            btStoryChoice1.setText(page.getChoice1().getButtonId());
 
             btStoryChoice1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     loadPage(page.getChoice1().getPageNumber());
+                    while (!pageStack.empty()){
+                         
+                    }
                 }
             });
 
             btStoryChoice2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(page.isFinalPage()){
-                        loadPage(0);
-                    }else {
                         loadPage(page.getChoice2().getPageNumber());
-                    }
+
                 }
             });
         }
 
     }
 
+    @Override
+    public void onBackPressed(){
+        // {0 1 2 3 4} // this is what in memory
+        pageStack.pop(); // remove latest number which is 4 and returns
+        // {0 1 2 3}
+
+        if(pageStack.isEmpty()){
+            super.onBackPressed();
+        } else{
+            // [0 1 2]
+            loadPage(pageStack.pop()); // pop returns what is removed // pop returns what is just removed from page// remove page 1 and leave it as 0
+        }
+    }
     //mvp model view presenter //3 models // each rows eg. button tv and iv used for navigation // entire layout is third model
 }
