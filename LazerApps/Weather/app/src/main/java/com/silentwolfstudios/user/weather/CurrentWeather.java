64c1abc3ecd130c826ab2964410830ccf2952996
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-24 15:44:50 +0800

    Created new getCurrentWeather(String jsonData) which takes in String passed from URL as JSON and return CurrentWeather while passing the data taken from the JSONData. Created new CurrentWeather class that will take in any value get and passed in from the JSONdata object.

---
+++ LazerApps/Weather/app/src/main/java/com/silentwolfstudios/user/weather/CurrentWeather.java
@@ -0,0 +1,32 @@
+package com.silentwolfstudios.user.weather;
+
+public class CurrentWeather {
+
+    private String summary;
+    private double temperature;
+    private double windSpeed;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+}
--- LazerApps/Weather/app/src/main/java/com/silentwolfstudios/user/weather/MainActivity.java
+++ LazerApps/Weather/app/src/main/java/com/silentwolfstudios/user/weather/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -18,17 +21,17 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     double latitude = 3.1280456;
     double longtitude = 101.7543843;
-    String apiKey ;
+    String apiKey;
     String forecast;
 
-    TextView tvWeather;
-
-
+    private TextView tvWeather;
+    private CurrentWeather weather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         apiKey = getResources().getString(R.string.apikey);
         forecast = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longtitude;
         tvWeather = findViewById(R.id.tvWeather);
@@ -37,38 +40,44 @@ public class MainActivity extends AppCompatActivity {
         Request request = new Request.Builder().url(forecast).build();
         Call call = client.newCall(request);
         call.enqueue(new Callback() {
-                         @Override
-                         public void onFailure(Call call, IOException e){
-                             alertDialog();
-                         }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                alertDialog();
+            }
 
-                         @Override
-                         public void onResponse(Call call, Response response) throws IOException {
-                            try{
-                                if (response.body()!=null) {
-                                    Log.d("Testing", response.body().string());
-                                }
-
-                                if(response.isSuccessful()){
-                                    Log.d("Response", "Response is successful"); // this is running in background thread thus we can't kust use Toast
-                                    runOnUiThread(new Runnable() { // tell the UI thread to call from the background thread
-                                        @Override
-                                        public void run() { // Toast
-                                            tvWeather.setText("Testing");
-                                        }
-                                    });
-                                }
-                                else{
-                                    Log.e("Response", "Response not successful");
-
-                                }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.body() != null) {
+                       getCurrentWeather(response.body().string()); // calling response body .string twice crashes the app, you can't request the body twice
+                    }
+                    if (response.isSuccessful()) {
+                        Log.d("Response", "Response is successful"); // this is running in background thread thus we can't kust use Toast
+                        runOnUiThread(new Runnable() { // tell the UI thread to call from the background thread
+                            @Override
+                            public void run() { // Toast
+                                tvWeather.setText("Testing");
                             }
-                            catch (IOException e){
-                                Log.e("Error Happenning", "IOException error");
+                        });
+                    } else {
+                        Log.e("Response", "Response not successful");
+                    }
+                } catch (IOException e) {
+                    Log.e("Error Happenning", "IOException error");
 
-                            }
-                         }
-                     });
+                } catch (JSONException e){ //exeption caught when passed in data is not JSON
+                    Log.e("Error Happening", "JSONException error");
+                }
+            }
+        });
+    }
+
+    private CurrentWeather getCurrentWeather(String jsonData) throws JSONException{ // throws exeptiom when passed data is not json format
+        JSONObject forecast = new JSONObject(jsonData);
+        double lat = forecast.getDouble("latitude"); // get latitude's data from JSONOBject passed in
+        Log.d("lat value", Double.toString(lat));
+
+        return null;
     }
 
     private void alertDialog() {
